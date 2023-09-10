6cea536726547ddd1465f4c3d49e1b20a82a991d
sarthaksingh007 <100351826+sarthaksingh007@users.noreply.github.com>
2023-09-10 22:49:42 +0530

    new comment

---
+++ index.js
@@ -0,0 +1,46 @@
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const socketIO = require("socket.io");
+
+const app = express();
+const port = 4500 || process.env.PORT;
+app.use(cors());
+
+const users = [{}];
+
+app.get("/", (req, res) => {
+    res.send("Hell its working");
+})
+
+
+const server = http.createServer(app);
+
+const io = socketIO(server);
+
+io.on("connection", (socket) => {
+    console.log("new connection");
+
+    socket.on('joined', ({ user }) => {
+        users[socket.id] = user; //jo bhi connect karega uski ek unique id hogi always
+        console.log(`${user} has joined`);
+        socket.broadcast.emit('userJoined', { user: "Admin", message: `${users[socket.id]} has joined` });
+        socket.emit('welcome', { user: "Admin", message: `welcome to the chat,${users[socket.id]}` })
+    }) 
+
+    socket.on('message',({message,id})=>{
+        io.emit('sendMessage',{user:users[id],message,id});
+    })
+
+    socket.on('disconnect',()=>{ 
+        socket.broadcast.emit('leave',{user:"Admin",message:`${users[socket.id]}, has left`});
+        console.log(`user left`) ;
+    })    
+
+
+ 
+});
+
+server.listen(port, () => {
+    console.log(`server is running on http://localhost:${port}`);
+})
