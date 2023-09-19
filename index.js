06884fa962d40b601397ed80bb0b3d5525d18771
sarthaksingh007 <100351826+sarthaksingh007@users.noreply.github.com>
2023-09-19 20:58:32 +0530

    update port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const socketIO = require("socket.io");
 
 const app = express();
-const port = process.env.PORT || 4500;
+const port = 4500 || 8000;
 app.use(cors());
 
 const users = [{}];
